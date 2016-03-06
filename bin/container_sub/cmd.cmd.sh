ff1459d6e3f305c0cac3f8d44a07ad529206d356
Diko the tech slave <diko316@gmail.com>
2016-03-07 04:03:17 +0800

    added option in "cmd" command to use bin/sh by applying -u parameter

--- bin/container_sub/cmd.cmd.sh
+++ bin/container_sub/cmd.cmd.sh
@@ -3,7 +3,26 @@
 
 CONTAINER=$(container info -a -m)
 
+######################
+# load defaults
+######################
+. "${SUBCOMMAND_DIR}/docker_defaults.sh"
 
-if [ "${CONTAINER}" ]; then
+######################
+# options
+######################
+COMMAND_OPTIONS="
+
+# use bin/sh 
+-u UNIX_SHELL 0
+
+"
+
+. "${SUBCOMMAND_DIR}/command_options_process.sh" $*
+
+
+if [ "${UNIX_SHELL_HAS_VALUE}" ]; then
+    docker exec -t -i "${CONTAINER}" /bin/sh
+else
     docker exec -t -i "${CONTAINER}" /bin/bash
 fi
